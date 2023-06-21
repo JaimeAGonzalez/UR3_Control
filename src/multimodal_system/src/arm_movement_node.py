f0ca1e0d737f7196a3ebe8d32af31013edff2fd0
Jaime Andrés Núñez González <67431910+JaimeAGonzalez@users.noreply.github.com>
2023-06-21 15:30:28 -0500

    Update arm_movement_node.py

--- src/multimodal_system/src/arm_movement_node.py
+++ src/multimodal_system/src/arm_movement_node.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 import sys
-import copy
 import rospy
+import time
 import moveit_commander
 import moveit_msgs.msg
 import geometry_msgs.msg
@@ -16,82 +16,14 @@ except:  # For Python 2 compatibility
     def dist(p, q):
         return sqrt(sum((p_i - q_i) ** 2.0 for p_i, q_i in zip(p, q)))
 
-moveit_commander.roscpp_initialize(sys.argv)
-rospy.init_node("move_group_python_interface", anonymous=True)
-
-robot = moveit_commander.RobotCommander()
-scene = moveit_commander.PlanningSceneInterface()
-
-group_name = "manipulator"
-move_group = moveit_commander.MoveGroupCommander(group_name)
-
-display_trajectory_publisher = rospy.Publisher(
-    "/move_group/display_planned_path",
-    moveit_msgs.msg.DisplayTrajectory,
-    queue_size=20,
-)
-
-# We can get the name of the reference frame for this robot:
-planning_frame = move_group.get_planning_frame()
-print("============ Planning frame: %s" % planning_frame)
-
-# We can also print the name of the end-effector link for this group:
-eef_link = move_group.get_end_effector_link()
-print("============ End effector link: %s" % eef_link)
-
-# We can get a list of all the groups in the robot:
-group_names = robot.get_group_names()
-print("============ Available Planning Groups:", robot.get_group_names())
-
-# Sometimes for debugging it is useful to print the entire state of the
-# robot:
-print("============ Printing robot state")
-print(robot.get_current_state())
-print("")
-
-# We get the joint values from the group and change some of the values:
-joint_goal = move_group.get_current_joint_values()
-joint_goal[0] = 0
-joint_goal[1] = -tau / 8
-joint_goal[2] = 0
-joint_goal[3] = -tau / 4
-joint_goal[4] = 0
-joint_goal[5] = tau / 6  # 1/6 of a turn
-
-# The go command can be called with joint values, poses, or without any
-# parameters if you have already set the pose or joint target for the group
-move_group.go(joint_goal, wait=True)
-
-# Calling ``stop()`` ensures that there is no residual movement
-move_group.stop()
-
-while True:
-    dr = input("Dame dir")
-    vertical = 0
-    horizontal = 0
-    if dr == 'W':
-        vertical=0.01
-    elif dr == 'S':
-        vertical=-0.01
-    elif dr == 'A':
-        horizontal=-0.01
-    elif dr == 'D':
-        horizontal=0.01
-
-    pose_goal = move_group.get_current_pose().pose
-    #pose_goal = geometry_msgs.msg.Pose()
-    #pose_goal.orientation.w = 1.0
-    pose_goal.position.x += vertical
-    pose_goal.position.y += horizontal
-    pose_goal.position.z += 0
-
-    move_group.set_pose_target(pose_goal)
-
-
-
 class arm_movement_node:
 
     def __init__(self):
+        # Flags
+        self.not_take = True
+        self.gripper_close = False
+        self.flag_coordinates = False
+        
         # Msg for the publish
         self.msg_gripper = String()
         self.msg_finish = Bool()
@@ -103,18 +35,163 @@ class arm_movement_node:
         self.subs_coordinates_goal = rospy.Subscriber('coordinates', Float64MultiArray, self.callback_coordinates_goal)
         self.rate = rospy.Rate(10) 
 
+        # Move-It
+        moveit_commander.roscpp_initialize(sys.argv)
+        rospy.init_node("move_group_python_interface", anonymous=True)
+        
+        robot = moveit_commander.RobotCommander()
+        scene = moveit_commander.PlanningSceneInterface()
+        
+        self.group_name = "manipulator"
+        self.move_group = moveit_commander.MoveGroupCommander(self.group_name)
+        
+        display_trajectory_publisher = rospy.Publisher(
+            "/move_group/display_planned_path",
+            moveit_msgs.msg.DisplayTrajectory,
+            queue_size=20,
+        )
+        
+        # We can get the name of the reference frame for this robot:
+        planning_frame = self.move_group.get_planning_frame()
+        print("============ Planning frame: %s" % planning_frame)
+        
+        # We can also print the name of the end-effector link for this group:
+        eef_link = self.move_group.get_end_effector_link()
+        print("============ End effector link: %s" % eef_link)
+        
+        # We can get a list of all the groups in the robot:
+        group_names = robot.get_group_names()
+        print("============ Available Planning Groups:", robot.get_group_names())
+        
+        # Sometimes for debugging it is useful to print the entire state of the
+        # robot:
+        print("============ Printing robot state")
+        print(robot.get_current_state())
+        print("")
+
         # Extract target object 
         while not rospy.is_shutdown():
-            a = 0
-          
+            if self.flag_coordinates == True and self.not_take == True:
+                self.move_to_object()
+                time.sleep(1)
+                self.down_to_object()
+                self.not_take = False
+            elif self.not_take == False and self.gripper_close == True:
+                self.move_to_object()
+                time.sleep(1)
+                self.goal_to_object()
+                time.sleep(1)
+                self.goal_down_to_object
+
     def callback_reset_system(self, msg_reset):
         # Extract the object to identify            
         self.reset = msg_reset.data
-
+        if self.rest == True:
+            # We get the joint values from the group and change some of the values:
+            joint_goal = self.move_group.get_current_joint_values()
+            joint_goal[0] = -tau/4
+            joint_goal[1] = 0
+            joint_goal[2] = -tau/4
+            joint_goal[3] = -tau/4
+            joint_goal[4] = tau/4
+            joint_goal[5] = tau/4  
+            
+            # The go command can be called with joint values, poses, or without any
+            # parameters if you have already set the pose or joint target for the group
+            self.move_group.go(joint_goal, wait=True)
+            
+            # Calling stop() ensures that there is no residual movement
+            self.move_group.stop()
+            self.reset = False
+            
+        else:
+            pass
+            
     def callback_coordinates_goal(self, msg_coordinates):
-          # Extract the object to identify            
-          self.goal_coordinates = msg_reset.data
-                  
+        # Extract the object to identify            
+        self.goal_coordinates = msg_coordinates.data
+        self.flag_coordinates = True
+
+    def move_to_object(self):
+        pose_goal = geometry_msgs.msg.Pose()
+
+        # Orientation
+        pose_goal.orientation.roll = 2.250
+        pose_goal.orientation.pitch = 2.136
+        pose_goal.orientation.yaw = -0.098
+
+        # Operational coordinates
+        pose_goal.position.x = self.goal_coordinates[0]
+        pose_goal.position.y = self.goal_coordinates[1]
+        pose_goal.position.z = self.goal_coordinates[2]
+
+        self.move_group.set_pose_target(pose_goal)
+        plan = self.move_group.go(wait=True)
+
+        # Calling `stop()` ensures that there is no residual movement
+        self.move_group.stop()
+        self.move_group.clear_pose_targets()
+
+    def down_to_object(self):
+        pose_goal = geometry_msgs.msg.Pose()
+
+        # Orientation
+        pose_goal.orientation.roll = 2.250
+        pose_goal.orientation.pitch = 2.136
+        pose_goal.orientation.yaw = -0.098
+
+        # Operational coordinates
+        pose_goal.position.x = self.goal_coordinates[0]
+        pose_goal.position.y = self.goal_coordinates[1]
+        pose_goal.position.z = self.goal_coordinates[2] - 132.43
+
+        self.move_group.set_pose_target(pose_goal)
+        plan = self.move_group.go(wait=True)
+
+        # Calling `stop()` ensures that there is no residual movement
+        self.move_group.stop()
+        self.move_group.clear_pose_targets()
+
+    def goal_to_object(self):
+        pose_goal = geometry_msgs.msg.Pose()
+
+        # Orientation
+        pose_goal.orientation.roll = 2.250
+        pose_goal.orientation.pitch = 2.136
+        pose_goal.orientation.yaw = -0.098
+
+        # Operational coordinates
+        pose_goal.position.x = 11.99
+        pose_goal.position.y = 467.18
+        pose_goal.position.z = 190.43
+
+        self.move_group.set_pose_target(pose_goal)
+        plan = self.move_group.go(wait=True)
+
+        # Calling `stop()` ensures that there is no residual movement
+        self.move_group.stop()
+        self.move_group.clear_pose_targets()
+
+    def goal_down_to_object(self):
+        pose_goal = geometry_msgs.msg.Pose()
+
+        # Orientation
+        pose_goal.orientation.roll = 2.250
+        pose_goal.orientation.pitch = 2.136
+        pose_goal.orientation.yaw = -0.098
+
+        # Operational coordinates
+        pose_goal.position.x = 11.99
+        pose_goal.position.y = 467.18
+        pose_goal.position.z = 190.43 - 132.43
+
+        self.move_group.set_pose_target(pose_goal)
+        plan = self.move_group.go(wait=True)
+
+        # Calling `stop()` ensures that there is no residual movement
+        self.move_group.stop()
+        self.move_group.clear_pose_targets()
+         
 if __name__ == '__main__':
     try:
         rospy.init_node('arm_movement_node')
